f9fc88d5acbcb86f148529e755754b4e148cc630
Vemulakonda.Divya <69154635+vemulakonda178@users.noreply.github.com>
2020-08-25 23:36:03 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from'react-dom';
+import './App.css';
+import State from './state';
+import NameHooks from './nameHooks'
+
+function App(){
+  
+  return (
+  <div> 
+  <div className="App">
+  <State />
+  <NameHooks />
+  
+    </div> 
+    </div>
+    );
+  }
+  export default App;
